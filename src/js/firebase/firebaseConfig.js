f069c14255069afe7cb72190d1a59a83306224f1
Alex <26582461+aheggie@users.noreply.github.com>
2020-10-16 20:37:11 -0400

    firebase initted, app setup on firebase console, firebase.json and npm scripts set for hosting, firebase config files available locally

---
+++ src/js/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDKJKxFyAXoGDwOIMRAfY3XCXyVQuMTSwk",
+  authDomain: "htg-development.firebaseapp.com",
+  databaseURL: "https://htg-development.firebaseio.com",
+  projectId: "htg-development",
+  storageBucket: "htg-development.appspot.com",
+  messagingSenderId: "1038503498333",
+  appId: "1:1038503498333:web:b5e8a04c3e57b891db33b4",
+  measurementId: "G-BNCJKHRSEK",
+};
+
+export default firebaseConfig;
