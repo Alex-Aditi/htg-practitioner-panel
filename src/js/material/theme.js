953b5dd214daf85c6159eb5ef5643314e775140a
Alex <26582461+aheggie@users.noreply.github.com>
2020-10-17 16:30:39 -0400

    MAterial UI theming and styling works on local build! First two acceptance criteria of Material theming story DONE!!!!! 🎨🎨🎨

--- src/js/material/theme.js
+++ src/js/material/theme.js
@@ -1,5 +1,11 @@
 import { createMuiTheme } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  // this is a test that theming works, can be removed
+  palette: {
+    secondary: purple,
+  },
+});
 
 export default theme;
