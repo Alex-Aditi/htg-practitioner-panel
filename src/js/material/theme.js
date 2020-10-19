5aef11925a5c40b5e98fa478d8bfb0aefda23a43
Aditi Ohri <didiohri@gmail.com>
2020-10-19 17:49:29 -0500

    introduce primary and secondary color schemes, copied from main site

--- src/js/material/theme.js
+++ src/js/material/theme.js
@@ -2,10 +2,19 @@ import { createMuiTheme } from "@material-ui/core";
 import { purple } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
-  // this is a test that theming works, can be removed
   palette: {
-    secondary: purple,
+    primary: {
+      main: 'c3abdc', // button border color
+      light: '#dec3fa', // button background-color
+      dark: '#5a2a88',
+    },
+    secondary: {
+      main: '#f2c944', // button background-color
+      light: '#ffe17e', // link color
+      dark: '#d5b13c', // button border color
+    },
   },
 });
+
 
 export default theme;
