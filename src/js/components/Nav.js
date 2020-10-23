3dd5d09e390275172a6949df7d588c2640187495
Aditi Ohri <didiohri@gmail.com>
2020-10-23 13:25:00 -0500

    apply grid to navbar

--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, AppBar, Toolbar, IconButton, Button, Link } from '@material-ui/core';
+import { makeStyles, AppBar, Toolbar, IconButton, Button, Link, Grid } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import cureLogo from "../../static/img/cure-logo.png";
@@ -7,15 +7,19 @@ import cureLogo from "../../static/img/cure-logo.png";
 const logo = {
   height: '3em',
   width: 'auto',
-  paddingTop: '0.3em'
+  alignSelf: 'center'
+  // paddingTop: '0.3em',
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
+  // root: {
+  //   flexGrow: 1,
+  // },
   menuButton: {
-    marginLeft: theme.spacing(2),
+    // marginLeft: theme.spacing(2),
+    '&:hover': {
+      backgroundColor: 'inherit'
+    }
   },
 }));
 
@@ -24,20 +28,38 @@ export default function Nav() {
 
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-        <img
-          src={cureLogo}
-          alt="Cure Concordia Logo"
-          style={logo}
-        />
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Link color="textSecondary" underline="none" href="#">Login</Link>
-          <Button variant="text">FR</Button>
-        </Toolbar>
-      </AppBar>
+      <Grid 
+        container 
+        direction="row"
+        alignContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12}>
+          <AppBar position="static">
+              <Toolbar>
+                <Grid item xs={10}>
+                  <img
+                    src={cureLogo}
+                    alt="Cure Concordia Logo"
+                    style={logo}
+                  />
+                </Grid>
+                <Grid item xs={1}>
+                  <Link color="textSecondary" underline="none" href="#">Login</Link>
+                </Grid>
+                <Grid item xs={1}>
+                  <Button variant="text">FR</Button>
+                </Grid>
+                <Grid item xs={1}>
+                  <IconButton edge="end" disableRipple={true} className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                  </IconButton>
+                </Grid>
+              </Toolbar>
+          </AppBar>
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
