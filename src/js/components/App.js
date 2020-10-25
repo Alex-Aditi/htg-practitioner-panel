2169bf5576c99e5d2e0365731d9e51c03a6f7938
Alex <26582461+aheggie@users.noreply.github.com>
2020-10-25 03:36:27 -0400

    conditionally show SignUpFlow when there is no user

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,11 +2,12 @@ import React, { useContext } from "react";
 import HeaderFooter from "./HeaderFooter";
 import Form from "./Form";
 import { UserContext } from "../providers/UserProvider";
+import SignUpFlow from "./SignUpFlow";
 const App = () => {
   const user = useContext(UserContext);
   return (
     <div>
-      <HeaderFooter>{user ? <Form /> : "there is no user"}</HeaderFooter>
+      <HeaderFooter>{user ? <Form /> : <SignUpFlow />}</HeaderFooter>
     </div>
   );
 };
---
+++ src/js/components/SignUpFlow.js
@@ -0,0 +1,13 @@
+import { Input } from "@material-ui/core";
+import React from "react";
+
+const SignUpFlow = () => {
+  return (
+    <div>
+      <Input placeholder="username"></Input>
+      <Input placeholder="password"></Input>
+    </div>
+  );
+};
+
+export default SignUpFlow;
