40a890491ee3b27ee5b8b844893a9104343cf8e3
Alex <26582461+aheggie@users.noreply.github.com>
2020-10-25 03:51:56 -0400

    fields and buttons are working with dummy behaviour

--- src/js/components/SignUpFlow.js
+++ src/js/components/SignUpFlow.js
@@ -1,11 +1,50 @@
-import { Input } from "@material-ui/core";
-import React from "react";
+import { Button, Input, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+
+const updateState = (stateFunction) => (event) =>
+  stateFunction(event.target.value);
 
 const SignUpFlow = () => {
+  const [newUserEmail, setNewUserEmail] = useState();
+  const [newUserPassword, setNewUserPassword] = useState();
+  const [returningUserEmail, setReturningUserEmail] = useState();
+  const [returningUserPassword, setReturningUserPassword] = useState();
   return (
     <div>
-      <Input placeholder="username"></Input>
-      <Input placeholder="password"></Input>
+      <Typography variant="h6">Sign Up</Typography>
+      <Input
+        placeholder="email"
+        value={newUserEmail}
+        onChange={updateState(setNewUserEmail)}
+      ></Input>
+      <Input
+        placeholder="password"
+        value={newUserPassword}
+        onChange={updateState(setNewUserPassword)}
+      ></Input>
+      <Button onClick={() => console.log({ newUserEmail, newUserPassword })}>
+        Sign Up
+      </Button>
+      <br></br>
+      <Typography variant="h6">Sign In</Typography>
+      <Input
+        placeholder="email"
+        value={returningUserEmail}
+        onChange={updateState(setReturningUserEmail)}
+      ></Input>
+      <Input
+        placeholder="password"
+        value={returningUserPassword}
+        onChange={updateState(setReturningUserPassword)}
+      ></Input>
+      <Button
+        onClick={() => {
+          alert(returningUserEmail);
+          alert(returningUserPassword);
+        }}
+      >
+        Sign In
+      </Button>
     </div>
   );
 };
