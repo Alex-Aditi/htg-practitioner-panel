eb8da3df73afc4d9781a91ab55d0291bf0094657
Alex <26582461+aheggie@users.noreply.github.com>
2020-10-25 03:33:23 -0400

    show Form conditional on user existing

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import HeaderFooter from "./HeaderFooter";
 import Form from "./Form";
+import { UserContext } from "../providers/UserProvider";
 const App = () => {
+  const user = useContext(UserContext);
   return (
     <div>
-      <HeaderFooter>
-        <Form />
-      </HeaderFooter>
+      <HeaderFooter>{user ? <Form /> : "there is no user"}</HeaderFooter>
     </div>
   );
 };
