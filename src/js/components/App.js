4647eff99cb2ae6b974e9bda82dff3eb92a5d993
Aditi Ohri <didiohri@gmail.com>
2020-10-27 18:07:53 -0500

    include log in and sign up form in App.js

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,5 @@
-import React from "react";
-import { Button, makeStyles, Typography } from "@material-ui/core";
-import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import React, { useContext } from "react";
+import { Typography } from "@material-ui/core";
 import HeaderFooter from "./HeaderFooter";
 import Form from "./Form";
 import { UserContext } from "../providers/UserProvider";
@@ -10,28 +8,18 @@ import SignUpFlow from "./SignUpFlow";
 const App = () => {
   const user = useContext(UserContext);
   return (
-      <HeaderFooter>
-        <div className="header">
-          <div className="inner home">
-            <Typography variant="h1" id="announce">
-              Welcome to the Practitioner Panel
-            </Typography>
-            <Button color="primary" id="seek">
-              THIS IS A THEMED BUTTON USING PRIMARY COLOR STYLE FROM src/js/theme.js
-            </Button>
-            <Button color="secondary" id="offer">
-              <InsertEmoticonIcon />
-              THIS IS A THEMED BUTTON USING SECONDARY COLOR STYLE FROM
-              src/js/theme.js
-            </Button>
-            <Button className={classes.borderButton} id="newsletter">
-              THIS IS A CUSTOM-sTYLED BUTTON USING LOCALLY DEFINED STYLES IN
-              js/components/App.js
-            </Button>
-        {user ? <Form /> : <SignUpFlow />}
+    <HeaderFooter>
+      <div className="header">
+        <div className="inner home">
+          <Typography variant="h1" id="announce">
+            Welcome to the Practitioner Panel
+          </Typography>
+          <div id="seek">
+            {user ? <Form /> : <SignUpFlow />}
           </div>
         </div>
-      </HeaderFooter>
+      </div>
+    </HeaderFooter>
   );
 };
 
