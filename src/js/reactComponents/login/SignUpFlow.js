3e547988a0f258de77de6f7b9546a2686fa1112c
Alex <26582461+aheggie@users.noreply.github.com>
2020-12-01 20:36:26 -0500

    lavender image importing correctly

--- src/js/reactComponents/login/SignUpFlow.js
+++ src/js/reactComponents/login/SignUpFlow.js
@@ -3,12 +3,15 @@ import React from "react";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 
+import lavenderJpg from "../../../static/img/header.jpg";
+
 const SignUpFlow = () => {
   return (
     <div>
       <Typography variant="h1" id="announce">
         Welcome to the Practitioner Panel
       </Typography>
+      <img src={lavenderJpg} alt="lavender"></img>
       <SignUp />
       <br></br>
       <SignIn />
