e88236055b9785e03801c15f99bed5bfa3ef9335
Aditi Ohri <didiohri@gmail.com>
2020-12-01 20:21:57 -0600

    import Card and center existing div

--- src/js/reactComponents/login/SignUpFlow.js
+++ src/js/reactComponents/login/SignUpFlow.js
@@ -1,26 +1,32 @@
-import { Typography } from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
+
 import React from "react";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
+import Card from "../UI/customComponents/SquareCard";
 
 import lavenderJpg from "../../../static/img/header.jpg";
 
-
-
 const SignUpFlow = () => {
   return (
-    <div
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justify="center"
       style={{
         background: `url(${lavenderJpg}) no-repeat top center/100% fixed`,
       }}
     >
-      <Typography variant="h1" id="announce" style={{ margin: "0 auto" }}>
-        Welcome to the Practitioner Panel
-      </Typography>
-      <SignUp />
-      <br></br>
-      <SignIn />
-    </div>
+      <Grid item xs={12}>
+        <Typography variant="h1" id="announce" style={{ margin: "0 auto" }}>
+          Welcome to the Practitioner Panel
+        </Typography>
+        <SignUp />
+        <br></br>
+        <SignIn />
+      </Grid>
+    </Grid>
   );
 };
 
