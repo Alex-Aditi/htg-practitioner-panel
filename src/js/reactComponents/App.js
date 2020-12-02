e05c11a5cf8d0d5d8cc09f46eb8b341e98528a0a
Alex <26582461+aheggie@users.noreply.github.com>
2020-12-01 20:05:14 -0500

    Welcome Typography moved into SignUpFlow; App now just shows Form or SignUpFlow based on user existance

--- src/js/reactComponents/App.js
+++ src/js/reactComponents/App.js
@@ -1,38 +1,12 @@
 import React, { useContext } from "react";
-import { Typography } from "@material-ui/core";
 import HeaderFooter from "./navigation/HeaderFooter";
 import Form from "./form/Form";
 import { UserContext } from "./providers/UserProvider";
 import SignUpFlow from "./login/SignUpFlow";
 
-import SquareCard from "./UI/customComponents/SquareCard";
-import ArrowBackward from "./UI/customComponents/ArrowBackward";
-import ArrowForward from "./UI/customComponents/ArrowForward";
-
 const App = () => {
   const user = useContext(UserContext);
-  return (
-    <HeaderFooter>
-      <div className="header">
-        <div className="inner home">
-          <Typography variant="h1" id="announce">
-            Welcome to the Practitioner Panel
-          </Typography>
-          <div id="seek">{user ? <Form /> : <SignUpFlow />}</div>
-          {/* <div id="offer"> */}
-          {/* <SquareCard title="Test Card">
-              <Typography>
-                This card is a sample of the cards that will be included in the
-                form.
-              </Typography>
-              <ArrowBackward text="Back" href="#" />
-              <ArrowForward text="Next" href="#" />
-            </SquareCard> */}
-          {/* </div> */}
-        </div>
-      </div>
-    </HeaderFooter>
-  );
+  return <HeaderFooter>{user ? <Form /> : <SignUpFlow />}</HeaderFooter>;
 };
 
 export default App;
--- src/js/reactComponents/login/SignUpFlow.js
+++ src/js/reactComponents/login/SignUpFlow.js
@@ -1,3 +1,4 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
@@ -5,6 +6,9 @@ import SignUp from "./SignUp";
 const SignUpFlow = () => {
   return (
     <div>
+      <Typography variant="h1" id="announce">
+        Welcome to the Practitioner Panel
+      </Typography>
       <SignUp />
       <br></br>
       <SignIn />
