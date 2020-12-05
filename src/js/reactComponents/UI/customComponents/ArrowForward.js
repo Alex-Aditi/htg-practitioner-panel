a99e53950e8dad7ea942bc787469d2cca0eab269
Aditi Ohri <didiohri@gmail.com>
2020-12-04 20:35:09 -0600

    refactor css jsx to be MUI makeStyles class

--- src/js/reactComponents/UI/customComponents/ArrowForward.js
+++ src/js/reactComponents/UI/customComponents/ArrowForward.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { Icon } from "@material-ui/core";
-import { Typography } from "@material-ui/core";
+import { Icon, Typography, makeStyles } from "@material-ui/core";
 import StyledIcon from "./StyledIcon";
 
-const textStyle = {
-  color: "#000",
-  textAlign: "right",
-  letterSpacing: "0.05em",
-  padding: "2px 6px 0px 0px",
-};
+const useStyles = makeStyles({
+  root: {
+    color: "#000",
+    textAlign: "right",
+    letterSpacing: "0.05em",
+    padding: "2px 6px 0px 0px",
+  },
+});
 
 export default function ArrowForward({ text, href }) {
+  const classes = useStyles();
   return (
     <StyledIcon href={href}>
-      <Typography variant="h6" style={textStyle}>
+      <Typography variant="h6" className={classes.root}>
         {text}
       </Typography>
       <Icon className="fas fa-arrow-right"></Icon>
