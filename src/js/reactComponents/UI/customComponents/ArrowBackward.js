763ea424d83180c0c5168fdf0c3380dedddaf265
Aditi Ohri <didiohri@gmail.com>
2020-12-04 20:41:30 -0600

    comment about the css in the Arrow components

--- src/js/reactComponents/UI/customComponents/ArrowBackward.js
+++ src/js/reactComponents/UI/customComponents/ArrowBackward.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Icon, Typography, makeStyles } from "@material-ui/core";
 import StyledIcon from "./StyledIcon";
+// this component creates an arrow icon that is a link with a text label
 
+// this creates a function that creates a classes object in ArrowBackward below
+// calling classes.root in className of a component applies those styles to that component
 const useStyles = makeStyles({
   root: {
     color: "#000",
@@ -15,6 +18,7 @@ export default function ArrowBackward({ label, href }) {
   const classes = useStyles();
   return (
     <StyledIcon href={href}>
+      {/* the icon below is styled using the FontAwesome library - that is what fas fa- denotes*/}
       <Icon className="fas fa-arrow-left"></Icon>
       <Typography variant="h6" className={classes.root}>
         {label}
--- src/js/reactComponents/UI/customComponents/ArrowForward.js
+++ src/js/reactComponents/UI/customComponents/ArrowForward.js
@@ -3,8 +3,9 @@ import { Icon, Typography, makeStyles } from "@material-ui/core";
 import StyledIcon from "./StyledIcon";
 
 // this component creates an arrow icon that is a link with a text label
-// 
 
+// this creates a function that creates a classes object in ArrowForward below
+// calling classes.root in className of a component applies those styles to that component
 const useStyles = makeStyles({
   root: {
     color: "#000",
@@ -21,6 +22,7 @@ export default function ArrowForward({ label, href }) {
       <Typography variant="h6" className={classes.root}>
         {label}
       </Typography>
+      {/* the icon below is styled using the FontAwesome library - that is what fas fa- denotes*/}
       <Icon className="fas fa-arrow-right"></Icon>
     </StyledIcon>
   );
