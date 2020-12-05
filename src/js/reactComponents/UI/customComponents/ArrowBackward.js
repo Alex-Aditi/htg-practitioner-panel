03023b1b2c762149ea8d17e14eddd60b3d35b8c7
Aditi Ohri <didiohri@gmail.com>
2020-12-04 20:32:33 -0600

    refactor css jsx object to be Material UI makeStyles class

--- src/js/reactComponents/UI/customComponents/ArrowBackward.js
+++ src/js/reactComponents/UI/customComponents/ArrowBackward.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Icon } from "@material-ui/core";
-import { Typography } from "@material-ui/core";
+import { Icon, Typography, makeStyles } from "@material-ui/core";
 import StyledIcon from "./StyledIcon";
 
-const textStyle = {
-  color: "#000",
-  textAlign: "left",
-  letterSpacing: "0.05em",
-  padding: "0px 0px 2px 6px",
-};
+const useStyles = makeStyles({
+  root: {
+    color: "#000",
+    textAlign: "left",
+    letterSpacing: "0.05em",
+    padding: "0px 0px 2px 6px",
+  }
+});
 
 export default function ArrowBackward({ text, href }) {
+  const classes = useStyles();
   return (
     <StyledIcon href={href}>
       <Icon className="fas fa-arrow-left"></Icon>
-      <Typography variant="h6" style={textStyle}>
+      <Typography variant="h6" className={classes.root}>
         {text}
       </Typography>
     </StyledIcon>
