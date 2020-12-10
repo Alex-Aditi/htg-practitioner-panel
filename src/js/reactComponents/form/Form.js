6fdad84d1a2fa09b426aa625d2aa4794039a1e55
Alex <26582461+aheggie@users.noreply.github.com>
2020-12-09 21:18:35 -0500

    headings laid out

--- src/js/reactComponents/form/Form.js
+++ src/js/reactComponents/form/Form.js
@@ -1,8 +1,18 @@
 //this might end up being multiple files etc
+import { Typography } from "@material-ui/core";
 import React from "react";
 
 const Form = () => {
-  return <>FORM! 📋📋📋📋📋📋📋</>;
+  <>
+    <Typography variant="h3">Section 0: Professional Guidelines</Typography>
+    <Typography variant="h3">Section 1: Title</Typography>
+    <Typography variant="h3">Section 2: Contact Info</Typography>
+    <Typography variant="h3">Section 3: Professional Information</Typography>
+    <Typography variant="h3">Section 4: Personal Information</Typography>
+    <Typography variant="h3">Section 5: Services</Typography>
+    <Typography variant="h3">Section 6: Review & Submit</Typography>
+    <Typography variant="h3">Section 7: Thank You & Confirmation</Typography>
+  </>;
 };
 
 export default Form;
