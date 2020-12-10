1e493858e0559131164e7dbf3f3c7278d26298fc
Alex <26582461+aheggie@users.noreply.github.com>
2020-12-09 22:15:50 -0500

    Sections 1-4 sketched out

--- src/js/reactComponents/form/Form.js
+++ src/js/reactComponents/form/Form.js
@@ -4,12 +4,127 @@ import React from "react";
 
 const Form = () => {
   <>
-    <Typography variant="h3">Section 0: Professional Guidelines</Typography>
+    {/* Section 1 */}
     <Typography variant="h3">Section 1: Title</Typography>
+    {/*  */}
+    <Typography variant="h5">Professional Guidelines</Typography>
+    <p>text block to read</p>
+    {/*  */}
+    <p>Next Button</p>
+
+    {/* Section 2 */}
     <Typography variant="h3">Section 2: Contact Info</Typography>
+    {/*  */}
+    <Typography variant="h5">Name</Typography>
+    <p>Multiple short text inputs</p>
+    {/*  */}
+    <Typography variant="h5">Email</Typography>
+    <p>Individual short text input</p>
+    {/*  */}
+    <Typography variant="h5">Phone #</Typography>
+    <p>Multiple short text inputs</p>
+    {/*  */}
+    <Typography variant="h5">Website</Typography>
+    <p>Individual short text input</p>
+    {/*  */}
+    <Typography variant="h5">Social media platforms</Typography>
+    <p>Short text input with option to add or delete more of its type</p>
+    {/*  */}
+    <p>BAck/Next Button Group</p>
+
+    {/* Section 3 */}
     <Typography variant="h3">Section 3: Professional Information</Typography>
+    {/*  */}
+    <Typography variant="h5">Professon Permit</Typography>
+    <p>
+      Multiple choice inputs with conditional short text inputs depending on
+      selections made
+    </p>
+    {/*  */}
+    <Typography variant="h5">Profession Not Regulated</Typography>
+    <p>Multiple choice inputs</p>
+    {/*  */}
+    <Typography variant="h5">Associations</Typography>
+    <p>Multiple choice inputs</p>
+    {/*  */}
+    <p>BAck/Next Button Group</p>
+
+    {/* Section 4 */}
     <Typography variant="h3">Section 4: Personal Information</Typography>
+    {/*  */}
+    <Typography variant="h5">Ethnicity/Race</Typography>
+    <p>Individual short text input</p>
+    {/*  */}
+    <Typography variant="h5">Ethnicity/Race in French</Typography>
+    <p>Individual short text input</p>
+    {/*  */}
+    <Typography variant="h5">Languages</Typography>
+    <p>Multiple choice inputs with "other" short text input</p>
+    {/*  */}
+    <Typography variant="h5">Gender</Typography>
+    <p>Single choice inputs with "other" short text input</p>
+    {/*  */}
+    <Typography variant="h5">LGTBTQ2S+</Typography>
+    <p>Single choice inputs</p>
+    {/*  */}
+    <Typography variant="h5">Trans Identify</Typography>
+    <p>Single choice inputs</p>
+    {/*  */}
+    <p>BAck/Next Button Group</p>
+
+    {/* Section 5 */}
     <Typography variant="h3">Section 5: Services</Typography>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <Typography variant="h5"></Typography>
+    <p></p>
+    {/*  */}
+    <p>BAck/Next Button Group</p>
+
     <Typography variant="h3">Section 6: Review & Submit</Typography>
     <Typography variant="h3">Section 7: Thank You & Confirmation</Typography>
   </>;
