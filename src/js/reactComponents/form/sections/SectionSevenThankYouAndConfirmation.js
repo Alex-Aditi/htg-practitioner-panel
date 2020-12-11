10122532868d0ba973b1257cb69a53f3c4287721
Alex <26582461+aheggie@users.noreply.github.com>
2020-12-10 22:26:13 -0500

    SectionSeven

--- src/js/reactComponents/form/sections/SectionSevenThankYouAndConfirmation.js
+++ src/js/reactComponents/form/sections/SectionSevenThankYouAndConfirmation.js
@@ -1,7 +1,19 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 
 function SectionSevenThankYouAndConfirmation() {
-  return <div></div>;
+  return (
+    <>
+      <Typography variant="h3">Section 7: Thank You & Confirmation</Typography>
+      {/*  */}
+      <Typography variant="h5">Confirmation</Typography>
+      <p>text block to read</p>
+      {/*  */}
+      <Typography variant="h5">Thank You</Typography>
+      <p>text block to read</p>
+      {/*  */}
+    </>
+  );
 }
 
 export default SectionSevenThankYouAndConfirmation;
