e8c3f571e0b0f175dece785cd7b7eb3df1ad47c5
Alex <26582461+aheggie@users.noreply.github.com>
2020-12-10 22:23:56 -0500

    SectionFive

--- src/js/reactComponents/form/sections/SectionFiveServices.js
+++ src/js/reactComponents/form/sections/SectionFiveServices.js
@@ -1,7 +1,52 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
+import IndiShortText from "../inputs/IndiShortText";
+import LongText from "../inputs/LongText";
+import MultiChoice from "../inputs/MultiChoice";
+import MultiChoiceOther from "../inputs/MultiChoiceOther";
+import MultiShortText from "../inputs/MultiShortText";
+import SingleChoice from "../inputs/SingleChoice";
 
 function SectionFiveServices() {
-  return <div></div>;
+  return (
+    <>
+      <Typography variant="h3">Section 5: Services</Typography>
+      {/*  */}
+      <MultiChoice header="Who do you provide services to?" />
+      {/*  */}
+      <MultiChoiceOther header="Your Treatment Approaches" />
+      {/*  */}
+      <MultiChoiceOther header="Needs/Experiences" />
+      {/*  */}
+      <MultiShortText header="Address" />
+      {/*  */}
+      <SingleChoice header="Fixed address?" />
+      {/*  */}
+      <SingleChoice header="Remotely" />
+      {/*  */}
+      <MultiChoiceOther header="if remotely, how?" />
+      {/*  */}
+      <MultiChoice header="Accessible Facilities" />
+      {/*  */}
+      <IndiShortText header="Hourly rate" />
+      {/*  */}
+      <MultiChoice header="Payment methods?" />
+      {/*  */}
+      <SingleChoice header="Sliding scale?" />
+      {/*  */}
+      <SingleChoice header="Barter/trade" />
+      {/*  */}
+      <SingleChoice header="Insurance coverage" />
+      {/*  */}
+      <SingleChoice header="Trans attestation letters" />
+      {/*  */}
+      <LongText header="personal bio" />
+      {/*  */}
+      <LongText header="personal bio in french" />
+      {/*  */}
+      <p>Back/Next Button Group</p>
+    </>
+  );
 }
 
 export default SectionFiveServices;
