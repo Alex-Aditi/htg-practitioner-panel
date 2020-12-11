97fb9fa4e894be036537079ec29fc5fc9c820912
Alex <26582461+aheggie@users.noreply.github.com>
2020-12-10 22:25:05 -0500

    sectionSix

--- src/js/reactComponents/form/sections/SectionSixReviewAndSubmit.js
+++ src/js/reactComponents/form/sections/SectionSixReviewAndSubmit.js
@@ -1,7 +1,14 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 
 function SectionSixReviewAndSubmit() {
-  return <div></div>;
+  return (
+    <>
+      <Typography variant="h3">Section 6: Review & Submit</Typography>
+      {/*  */}
+      <p>Submit Button Group</p>
+    </>
+  );
 }
 
 export default SectionSixReviewAndSubmit;
