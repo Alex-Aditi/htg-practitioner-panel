e623d5c99f4bf660c9a229a0d58406f6fffe8e2b
Aditi Ohri <didiohri@gmail.com>
2020-12-11 19:56:30 -0600

    set default value of currentSection to 1 using useState

--- src/js/reactComponents/form/Form.js
+++ src/js/reactComponents/form/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionOneIntro from "./sections/SectionOneIntro";
 import SectionTwoContactInfo from "./sections/SectionTwoContactInfo";
 import SectionThreeProfessionalInformation from "./sections/SectionThreeProfessionalInformation";
@@ -8,7 +8,7 @@ import SectionSixReviewAndSubmit from "./sections/SectionSixReviewAndSubmit";
 import SectionSevenThankYouAndConfirmation from "./sections/SectionSevenThankYouAndConfirmation";
 
 const Form = () => {
-  let currentSection = Math.floor(Math.random() * 7) + 1;
+  const [currentSection, setCurrentSection] = useState(1);
   return (
     <>
     {currentSection === 1 && (
