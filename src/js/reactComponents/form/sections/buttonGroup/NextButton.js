0242f98a793474220c396bd251900a8cc377eecf
Alex <26582461+aheggie@users.noreply.github.com>
2020-12-11 21:51:34 -0500

    colors on buttons

--- src/js/reactComponents/form/sections/buttonGroup/NextButton.js
+++ src/js/reactComponents/form/sections/buttonGroup/NextButton.js
@@ -2,7 +2,9 @@ import { Button } from "@material-ui/core";
 import React from "react";
 
 const NextButton = ({ nextFunc }) => (
-  <Button onClick={nextFunc}>Next Section</Button>
+  <Button onClick={nextFunc} color="primary">
+    Next Section
+  </Button>
 );
 
 export default NextButton;
--- src/js/reactComponents/form/sections/buttonGroup/PrevButton.js
+++ src/js/reactComponents/form/sections/buttonGroup/PrevButton.js
@@ -2,7 +2,9 @@ import { Button } from "@material-ui/core";
 import React from "react";
 
 const PrevButton = ({ prevFunc }) => (
-  <Button onClick={prevFunc}>Previous Section</Button>
+  <Button onClick={prevFunc} color="secondary">
+    Previous Section
+  </Button>
 );
 
 export default PrevButton;
