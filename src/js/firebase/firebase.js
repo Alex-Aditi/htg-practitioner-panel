950cd7064e7616d654540e0ed6510691917c0e81
Aditi Ohri <didiohri@gmail.com>
2020-12-17 19:37:09 -0600

    comment auth import pattern in firebase.js

--- src/js/firebase/firebase.js
+++ src/js/firebase/firebase.js
@@ -1,9 +1,16 @@
+// firebase/app pulls in code to initiate communication with firebase
+// on it's own, you cannot use it to access services
 import firebase from "firebase/app";
+// calling firebase/auth allows us to pull out authentication services in a modular way
+// if we were to call another service, i.e. analytics you can do so like this: 
+// import "firebase/analytics"
+// documentation: https://firebase.google.com/docs/web/setup#using-module-bundlers
 import "firebase/auth";
 import firebaseConfig from "./firebaseConfig";
 
 firebase.initializeApp(firebaseConfig);
 
+// this only works because we imported auth above on line 8, after importing firebase/app
 const firebaseAuth = firebase.auth();
 
 export default firebase;
